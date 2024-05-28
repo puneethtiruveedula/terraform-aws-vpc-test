1b427bc2817fcadd02b3d3969179e07e67eebc0b
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-05-28 17:52:46 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "project_name" {
+    default = "expense"
+  
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "public_subnet_cidrs" {
+    default = ["10.0.3.0/24","10.0.4.0/24"]
+  
+}
+
+variable "private_subnet_cidrs" {
+    default = ["10.0.13.0/24","10.0.14.0/24"]
+  
+}
+
+variable "database_subnet_cidrs" {
+    default = ["10.0.22.0/24","10.0.23.0/24"]
+  
+}
+
+variable "is_peering_required" {
+    default = true
+  
+}
